0bc9cd3a23897150012497ea62a1a072e46f7d06
vineela8919 <vineelabv2005@gmail.com>
2024-10-13 20:57:29 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,18 @@
+)Write a program to check whether the given character is vowel or consonant.
+Input format:
+The input consist of a character
+Output format:
+The output consists of a below-given string
+“Vowel” / “Consonant” / “Not an alphabet”
+Sample Input:
+e
+Sample Output:
+Vowel
+
+a=input()
+if(a=='a' or a=='e' or a=='i' or a=='o' or a=='u'):
+print('Vowel')
+elif(a>'a' and 'a'<a):
+print("Consonant")
+else:
+print("Not an alphabet")
